100181432aea2d3c51c39b75e11113e56196aa42
Michel Avalos <maval@fiu.edu>
2024-04-10 00:41:20 -0400

    inital commit

---
+++ src/main/java/com/Team5/demo/BlackjackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Team5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Team5/demo/BlackjackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
