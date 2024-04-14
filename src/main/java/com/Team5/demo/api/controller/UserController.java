ed760a44b583f55310831ceafff786c9dc9bca01
Michel Avalos <maval@fiu.edu>
2024-04-14 19:36:40 -0400

    CORS CANCER

--- src/main/java/com/Team5/demo/api/controller/UserController.java
+++ src/main/java/com/Team5/demo/api/controller/UserController.java
@@ -6,9 +6,11 @@ import com.Team5.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -25,6 +27,7 @@ public class UserController {
         }
 
         else {
+            System.out.println("\"Login Failed! Invalid Username or Password!\"");
             return "Login Failed! Invalid Username or Password!";
         }
     }
